6742e9c06c2c807932f2b8bcef47a2782e3a99c7
NDK <dk.wofts@gmail.com>
2020-02-07 21:18:54 +0700

    test with member
    add a simple member array for test

---
+++ WebAppNo1/WebAppNo1/Controllers/MemberController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAppNo1.Models;
+
+namespace WebAppNo1.Controllers
+{
+    public class MemberController : ApiController
+    {
+        m_user[] members = new m_user[]
+        {
+            new m_user {user_id = 1, user_name = "DKhoa", password ="123", email ="DKhoa@123",
+                        phone ="0123456789", reg_date = DateTime.Now, is_active = false },
+            new m_user {user_id = 2, user_name = "TKhoa", password ="456", email ="TKhoa@456",
+                        phone ="0987654321", reg_date = DateTime.Now, is_active = false },
+            new m_user {user_id = 3, user_name = "NKhoa", password ="789", email ="NKhoa@789",
+                        phone ="0999999999", reg_date = DateTime.Now, is_active = false },
+        };
+
+        public IEnumerable<m_user> GetAllMember()
+        {
+            return members;
+        }
+
+        public IHttpActionResult GetMember(int id)
+        {
+            var member = members.FirstOrDefault(p => p.user_id == id);
+            if (member == null)
+                return NotFound();
+            else
+                return Ok(member);
+        }
+    }
+}
---
+++ WebAppNo1/WebAppNo1/Models/m_user.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace WebAppNo1.Models
+{
+    public class m_user
+    {
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime reg_date { get; set; }
+        public bool is_active { get; set; }
+
+        //public string ConnectionString { get; set; }
+        //public m_user(string connectionString)
+        //{
+        //    this.ConnectionString = connectionString;
+        //}
+
+        //private MySqlConnection GetConnection()
+        //{
+        //    return new MySqlConnection(ConnectionString);
+        //}
+    }
+}
