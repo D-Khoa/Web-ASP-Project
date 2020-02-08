1471e9eb86079e049fb3ca2312f97e4baa4c119b
NDK <dk.wofts@gmail.com>
2020-02-08 21:10:55 +0700

    test javascript

---
+++ WebAppNo1/WebAppNo1/scripts/MessageBox.js
@@ -0,0 +1,3 @@
+﻿var button = document.getElementById('clickme');
+setTimeout(function () { document.write("Hi!"); }, 3000);
+button.addEventListener('click', function () { alert("Hello World!"); });
