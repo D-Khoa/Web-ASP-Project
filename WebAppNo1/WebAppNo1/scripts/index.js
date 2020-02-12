7b241cee8f654cc1213548d12e90d83f40aa3a12
NDK <dk.wofts@gmail.com>
2020-02-12 23:22:20 +0700

    read data & write data
    read/write data firebase database OK

--- WebAppNo1/WebAppNo1/scripts/index.js
+++ WebAppNo1/WebAppNo1/scripts/index.js
@@ -1,8 +1,9 @@
 firebase.auth().onAuthStateChanged(function(user) {
 	if (user) {
 	    // User is signed in.
-	    document.getElementById("user_div").style.display = 'block';
-	    document.getElementById("login_div").style.display = 'none';
+	    document.getElementById("user_info_div").style.display = 'block';
+	    document.getElementById("main_log_div").style.display = 'none';
+	    document.getElementById("write_data").style.display = 'block';
 	    var user = firebase.auth().currentUser;
 	    if(user != null)
 	    {
@@ -11,11 +12,28 @@ firebase.auth().onAuthStateChanged(function(user) {
 	    }
 	} 
 	else {
-	    // No user is signed in.
-	    document.getElementById("user_div").style.display = 'none';
-	    document.getElementById("login_div").style.display = 'block';
+		// No user is signed in.
+		var btnCallLogin = document.getElementById("top_login_button");
+		btnCallLogin.onclick = function(){
+			document.getElementById("main_log_div").style.display = 'block';
+			document.getElementById("user_info_div").style.display = 'none';
+			document.getElementById("write_data").style.display = 'none';
+			document.body.style.background = 'rgba(0,0,0,0.5)';
+		}
 	}
 });
+
+function Register(){
+	var userEmail = document.getElementById("login_email").value;
+	var userPass = document.getElementById("login_pass").value;
+	firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // ...
+	  	window.alert("Error: " + errorMessage);
+	});
+}
 
 function Login() {
 	var userEmail = document.getElementById("login_email").value;
@@ -34,5 +52,35 @@ function Logout(){
 	  // Sign-out successful.
 	}).catch(function(error) {
 	  // An error happened.
+	});
+}
+
+function writeUserData() {
+	var name = document.getElementById("write_name").value;
+	var email = document.getElementById("write_email").value;
+	var pass = document.getElementById("write_pass").value;
+
+	firebase.database().ref('users/' + name).set({
+	    email: email,
+	    password : pass
+	}).catch(function(error){
+		var errorMessage = error.message;
+	  	window.alert("Error: " + errorMessage);
+	});
+	window.alert("successful: " + name);
+}
+
+function readUserData(){
+	var userId = firebase.auth().currentUser.uid;
+	var name = document.getElementById("write_name").value;
+	var email = document.getElementById("write_email").value;
+	var pass = document.getElementById("write_pass").value;
+
+	return firebase.database().ref('/users/' + name).once('value').then(function(snapshot) {
+	  email = (snapshot.val() && snapshot.val().email) || 'Empty';
+	  pass = (snapshot.val() && snapshot.val().password) || 'Empty';
+	  // ...
+	  document.getElementById("write_email").value = email;
+	  document.getElementById("write_pass").value = pass;
 	});
 }
