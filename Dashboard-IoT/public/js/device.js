68f0a7cf24d6292ef58c46850ab2f1e58ca5c08f
NDK <dk.wofts@gmail.com>
2020-02-23 23:11:41 +0700

    fix button
    - set size actutors and sensors

--- Dashboard-IoT/public/js/device.js
+++ Dashboard-IoT/public/js/device.js
@@ -33,10 +33,10 @@ function AddActutorBtn(actId){
 			UpdateUserActutor(actId, user.uid);
 			element.innerHTML = doc.data().name + "-" + actId;
 			if(doc.data().state){
-				element.className = "btn btn-large btn-success"
-				}
-				else{
-				element.className ="btn btn-large btn-inverse";
+				element.className = "btn btn-large btn-success btn-actutor"
+			}
+			else{
+				element.className ="btn btn-large btn-inverse btn-actutor";
 			}
 		}
 		else{
@@ -50,23 +50,23 @@ function AddActutorBtn(actId){
 			if(doc.exists){
 				element.innerHTML = doc.data().name + "-" + actId;
 				if(!doc.data().state){
-					element.className = "btn btn-large btn-success";					
+					element.className = "btn btn-large btn-success btn-actutor";					
 				}
 				else{
-					element.className ="btn btn-large btn-inverse";
+					element.className ="btn btn-large btn-inverse btn-actutor";
 				}
 				docRef.doc("actutors/" + actId).update({	
-	state : !doc.data().state
-	}).then(function(){
-	console.log("Update successful!");
-	}).catch(function(error){
-	console.log("Got an error: ", error);
-	});
-	}}).catch(function(error){console.log("Got an error: ", error)});
+					state : !doc.data().state
+					}).then(function(){
+					console.log("Update successful!");
+					}).catch(function(error){
+					console.log("Got an error: ", error);
+				});
+			}}).catch(function(error){console.log("Got an error: ", error)});
 	}	
-	}
-	
-	function AddSensorLabel(sensorID){
+}
+
+function AddSensorLabel(sensorID){
 	var docRef = firebase.firestore();
 	var user = firebase.auth().currentUser;
 	var element = document.createElement('span');
@@ -75,26 +75,26 @@ function AddActutorBtn(actId){
 	var isExist = true;
 	element.id = sensorID;	
 	docRef.doc("sensors/" + sensorID).onSnapshot(function(doc){
-	if(doc && doc.exists){
-	UpdateUserSensor(sensorID,user.uid);
-	element.innerHTML = doc.data().name + ": " + doc.data().value;
-	if(doc.data().state){
-	element.className = "label label-success";
-	}
-	else{
-	element.className ="label";
-	}
-	}
-	else{
-	console.log("This sensor is not exists!");
-	isExist = false;
-	}
+		if(doc && doc.exists){
+			UpdateUserSensor(sensorID,user.uid);
+			element.innerHTML = doc.data().name + ": " + doc.data().value;
+			if(doc.data().state){
+				element.className = "label label-success label-sensor";
+			}
+			else{
+				element.className ="label label-sensor";
+			}
+		}
+		else{
+			console.log("This sensor is not exists!");
+			isExist = false;
+		}
 	});
 	if(isExist)	sensorField.appendChild(element);
 	else element.parentNode.removeChild(element);
-	}
-	
-	function UpdateUserActutor(itemID, userid){
+}
+
+function UpdateUserActutor(itemID, userid){
 	var docRef = firebase.firestore();
 	var itemArray = null;
 	docRef.doc("users/" + userid).get().then(function(doc){
@@ -168,4 +168,4 @@ function AddActutorBtn(actId){
 	}).catch(function(error){
 	console.log("Got an error: ", error);
 	});
-	}		
+	}			
