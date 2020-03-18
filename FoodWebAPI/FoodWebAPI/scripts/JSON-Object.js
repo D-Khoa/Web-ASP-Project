c3c0665441bdaa5e85d7b538d8ecb1c9255f60a7
NDK <dk.wofts@gmail.com>
2020-03-18 22:34:50 +0700

    Test API example
    - Use simple GET method for get food data and show it into html (jQuery)

---
+++ FoodWebAPI/FoodWebAPI/scripts/JSON-Object.js
@@ -0,0 +1,12 @@
+﻿function GET(tbl) {
+    const id = document.querySelector('#dataid').value;
+    var myurl = 'http://localhost:50485/api/' + tbl + '/' + id;
+    $.getJSON(myurl, function (data) {
+
+        var name = `${data.name}`;
+        var price = `${data.price}`;
+
+        $(".dataname").html('Name: '+ name);
+        $(".dataprice").html('Price: '+ price);
+    });
+}
