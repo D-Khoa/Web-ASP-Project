bf3693466f24ea0c1717b0eb17ffdfc661802577
DangKhoa <dk.wofts@gmail.com>
2020-09-21 22:14:47 +0700

    update

---
+++ IFM_MES_WebControl/IFM_ManufacturingExecutionSystems/Controllers/PLCManagementController.cs
@@ -0,0 +1,30 @@
+﻿using IFM_ManufacturingExecutionSystems.Models.Database;
+using IFM_ManufacturingExecutionSystems.Models.MVC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace IFM_ManufacturingExecutionSystems.Controllers
+{
+    public class PLCManagementController : Controller
+    {
+        private readonly string baseURI;
+        private readonly IConfiguration _config;
+
+        public PLCManagementController(IConfiguration configuration)
+        {
+            _config = configuration;
+            baseURI = GlobalVariable.BaseURI;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IFM_MES_WebControl/IFM_ManufacturingExecutionSystems/Models/MVC/PLCManagement.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IFM_ManufacturingExecutionSystems.Models.MVC
+{
+    public class PLCManagement
+    {
+        [Key]
+        public int plcId { get; set; }
+        public string plcCode { get; set; }
+        public string plcAddress { get; set; }
+        public string model { get; set; }
+        public string line { get; set; }
+        public string process { get; set; }
+        public bool status { get; set; }
+    }
+}
